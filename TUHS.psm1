9cf312b8c64c524b3a87c745a7632c2b0e7d6992
Zachary Hilliker <Zachary.Hilliker@tuhs.temple.edu>
2023-07-27 13:52:40 -0400

    Initial function

--- TUHS.psm1
+++ TUHS.psm1
@@ -1,27 +1,11 @@
 
-function Invoke-SqlCommand{
-    [OutputType([System.Data.DataTable])]
-    Param(
-        [Parameter(Mandatory)]
-        [string]$Query,
-        [string]$ServerInstance,
-        [string]$Database
-    )
-    
-    Try{
-      $SqlConnection = New-Object System.Data.SqlClient.SqlConnection  
-      #  $SqlConnection.ConnectionString = "Data Source=tcp:rmxprod.database.windows.net,1433;Initial Catalog=rhythmedix;Authentication=Active Directory Integrated;"
-        $SqlCmd = New-Object System.Data.SqlClient.SqlCommand
-        $sqlconnection.Open()
-        $sqlcmd = $sqlconnection.CreateCommand()
-        $sqlcmd.CommandText = $query
-        $rdr = $sqlcmd.ExecuteReader()
-        $dt = [System.Data.DataTable]::New()
-        $dt.Load($rdr)
-        $sqlconnection.Close()
-    }
-    Catch{
-        Throw $_
-    }
-    @(,$dt)
+function Find-ADGroup{
+
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [String]
+    $Search
+)
+    get-adgroup -filter {Name -like "*$Search*"}
 }
