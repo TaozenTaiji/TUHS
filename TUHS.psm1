827755cd938bedcef4798fcfba83d10bda8e3ee1
Zachary Hilliker <Zachary.Hilliker@tuhs.temple.edu>
2023-08-04 12:03:49 -0400

    syntax

--- TUHS.psm1
+++ TUHS.psm1
@@ -9,3 +9,47 @@ param (
 )
     get-adgroup -filter {Name -like "*$Search*"}
 }
+
+function Install-SCCM
+{
+    Set-Location 'C:\Program Files (x86)\Microsoft Endpoint Manager\AdminConsole\bin'
+    Import-Module .\ConfigurationManager.psd1
+    new-PSDrive -Name "SCCM" -PSProvider "CMSite" -root "tssccmmgtprd01.tuhs.prv" -Description "Primary Site"
+    Update-Help -Module ConfigurationManager
+}
+
+function Get-SCCMPackage
+{
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [String]
+        $PackageID
+    )
+    Set-location SCCM:
+
+    try
+     {
+        Get-CMPackage $PackageID
+    }
+    catch 
+    {
+        try 
+        {
+            Get-CMDeploymentPackage $PackageID
+        }
+        catch 
+        {
+            try 
+            {
+                Get-CMSoftwareUpdateDeploymentPackage $PackageID
+            }
+            catch 
+            {
+                Get-CMDriverPackage $PackageID
+            }
+        }
+    }
+}
+    
+    
